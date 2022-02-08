5390ac8cc2997fb30c4e9e4791f23186060ea9d4
Muskan-coder719 <82265381+Muskan-coder719@users.noreply.github.com>
2022-02-08 17:55:49 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float a,b,c;
+	std::cout<<"enter 2 numbers : ";
+	cin>>a>>b ;
+	c=a*b;
+	std::cout<<"product of numbers is : "<<c;
+	return 0;
+}
+
+
+output :
+enter 2 numbers = 3.4  5.5
+product of numbers is : 18.7
