12989f9ed76bbcd36ca338e23fd27c29b78c4969
Muskan-coder719 <82265381+Muskan-coder719@users.noreply.github.com>
2022-02-08 17:53:54 +0530

    Create size_data_types.cpp

---
+++ size_data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() 
+{    
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    return 0;
+}
+
+
+OUTPUT :
+size of char: 1 byte 
+size of int: 4 bytes
+size of float: 4 bytes 
+size of double: 8 bytes 
